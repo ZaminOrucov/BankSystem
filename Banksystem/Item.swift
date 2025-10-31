c8480d19b5e69069b638d5a0f6d1e05cadb54a79
ZaminOrucov <zamin.h.orujov@gmail.com>
2025-10-31 15:37:29 +0400

    Initial Commit

---
+++ Banksystem/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Banksystem
+//
+//  Created by Zamin Orucov on 10/31/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BanksystemTests/BanksystemTests.swift
@@ -0,0 +1,17 @@
+//
+//  BanksystemTests.swift
+//  BanksystemTests
+//
+//  Created by Zamin Orucov on 10/31/25.
+//
+
+import Testing
+@testable import Banksystem
+
+struct BanksystemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
